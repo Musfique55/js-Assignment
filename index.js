61e9f642a0adeb1325bd5c7dfa9dcd6b5509c101
Musfique55 <mdmusfiquepatwary@gmail.com>
2024-02-08 15:16:01 +0600

    completed all requiremnts

---
+++ index.js
@@ -0,0 +1,93 @@
+function calculateMoney(ticketSale){
+    // You have to write your code here
+    if(ticketSale < 0){
+        return '"Please Provide Positive Amount"';
+    }
+    const ticketPrice = 120;
+    const GuardFee = 500;
+    const staffCost = 50;
+    const staffs = 8;
+    const income = ticketSale * ticketPrice;
+    const totalCost = GuardFee + staffs * staffCost;
+    const remainingTaka = income - totalCost;
+    return remainingTaka;
+}
+
+
+function checkName(name) {
+    //write your code here
+    if(typeof name !== 'string'){
+        return 'invalid';
+    }
+    const lowerCase = name.toLowerCase();
+    const last = lowerCase.charAt(lowerCase.length-1);
+    if(last.includes('a') || last.includes('y') || last.includes('i') || last.includes('e') || last.includes('o') || last.includes('u') || last.includes('w')){
+        return 'Good Name';
+    }else{
+        return 'Bad Name';
+    }
+}
+
+
+function deleteInvalids(array) {
+    // You have to write your code here
+    if(!Array.isArray(array)){
+        return '"Invalid Array"';
+    }
+    let newArr = [];
+    for(let ar of array){
+        if(typeof ar === 'number' && !isNaN(ar)){
+            newArr.push(ar);
+        }
+    }
+    return newArr;
+}
+
+
+function password(obj) {
+    //write your code here
+    const length = Object.keys(obj);
+    if(length.length !== 3){
+        return 'invalid';
+    }
+    
+    const name = obj.name;
+    const birthYear = obj.birthYear ; 
+    const siteName = obj.siteName[0].toUpperCase() + obj.siteName.slice(1).toLowerCase();
+    const string = birthYear.toString();
+   
+    if(string.length < 4){
+        return 'invalid'
+    }
+    const generatePass = siteName + '#' + name + '@' + string;
+    return generatePass;
+}
+
+
+function monthlySavings(arr , livingCost) {
+    // You have to write your code here
+    if(!Array.isArray(arr)){
+        return 'Invalid input';
+    }else if(typeof livingCost !== 'number'){
+        return 'Invalid input';
+    }
+
+    let income = 0;
+    let incomeWithTax = 0;
+    for(const ar of arr){
+        if(ar >= 3000){
+            incomeWithTax+= ar;
+            const tax = ar * 20 /100;
+            incomeWithTax = incomeWithTax  -  tax;
+            income =incomeWithTax + ar;        
+        }else{
+            income+= ar;
+        }
+    }
+    const totalSavings  = income - livingCost;
+    if(totalSavings >= 0){
+        return totalSavings;
+    } else{
+        return '"earn more"';
+    }
+}
